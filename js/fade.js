6a0d39cc594c07b4edd6e8b4c76881100b0f1067
Kyle Rector <kylearector@outlook.com>
2021-09-27 22:35:07 -0400

    Updating fade routine

--- js/fade.js
+++ js/fade.js
@@ -1,5 +1,18 @@
 function loadFade() {
+    const linkIds = [
+        "github-link",
+        "twitter-link",
+        "hackaday-link",
+        "linkedin-link"
+    ]
+
     document.getElementById("name-header").style.opacity = 1
-    setTimeout(() => {document.getElementById("subtitle-header").style.opacity = 1}, 500)
-    setTimeout(() => {document.getElementById("links").style.opacity = 1}, 1000)
+    setTimeout(() => {document.getElementById("subtitle-header").style.opacity = 1}, 750)
+    setTimeout(() => {document.getElementById("links").style.opacity = 1}, 1500)
+
+    let timeout = 3000
+    for (const linkId of linkIds) {
+        timeout += 250
+        setTimeout(() => {document.getElementById(linkId).style.opacity = 1}, timeout)
+    }
 }
